163fa2392fa172283da29bbb5b1ac96530fa47c2
Moloko <molokodineo04@gmail.com>
2026-04-13 17:10:27 +0200

    welcoming voice record

---
+++ CybersecurityBot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Media;
+using System.Threading;
+
+namespace CybersecurityAwarenessBot
+{
+    public class CybersecurityBot
+    {
+        private User currentUser = new User();
+        private SoundPlayer voicePlayer;   
+
+        public CybersecurityBot()
+        {
+            try
+            {
+                voicePlayer = new SoundPlayer("greetings.wav");
+            }
+            catch
+            {
+                Console.WriteLine("Warning: greetings.wav file not found.");
+            }
+        }
+
+        
+
+
+        private void PlayVoiceGreeting()
+        {
+            if (voicePlayer != null)
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" Playing greeting voice message...");
+                    Console.ResetColor();
+
+                    voicePlayer.Play();
+                    Thread.Sleep(4500);        
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Could not play voice: {ex.Message}");
+                }
+            }
+        }
+
+        
+
+
+
